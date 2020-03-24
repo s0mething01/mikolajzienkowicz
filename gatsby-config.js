2ea9ad75a0106e9c8ae3819123f46f6f214512d2
Mikołaj <miki70707@interia.pl>
2020-03-24 03:27:37 +0100

    webmanifest added and  fav icons added

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,32 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        // name: "GatsbyJS",
+        // short_name: "GatsbyJS",
+        // start_url: "/",
+        // background_color: "#6b37bf",
+        // theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        // display: "standalone",
+        // icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        // crossOrigin: `use-credentials`,
+        name: "MZWeb",
+        short_name: "MZWeb",
+        start_url: "/",
+        background_color: "#6c63ff",
+        theme_color: "#6c63ff",
+        display: "standalone",
+        icon: "src/assets/meta/android-chrome-96x96.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,10 @@ import { theme } from '../theme/theme';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../theme/GlobalStyle';
 import { Helmet } from "react-helmet";
-
+import appletouchicon from "../assets/meta/apple-touch-icon.png";
+import favIconLarge from "../assets/meta/favicon-32x32.png";
+import favIconSmall from "../assets/meta/favicon-16x16.png";
+import maskIcon from "../assets/meta/safari-pinned-tab.svg";
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
@@ -15,11 +18,10 @@ const Layout = ({ children }) => {
                 <title>MikolajZienkowicz</title>
                 <meta name="description" content="Jestem młodym twórcą stron internetowych, jeśli potrzbujesz małej strony dla twojej firmy o schludnym wyglądzie, możliwością edycji treści, która dostosowana jest do wszystkich urządzeń, to dobrze trafiłeś!" />
                 <meta name="keywords" content="strona, strona internetowa, mikołaj, zienkowicz, mikolaj, developer, tanio, szybko, piękna, funkcjonalna, zarządzanie, treścią, web, reklama, internetowa, tworzenie, przygotowanie, projekt, solidnie, responsive, cms, responsywność, dopasowana, urządzenia, różne " />
-                <link rel="apple-touch-icon" sizes="76x76" href="/static/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-                <link rel="manifest" href="/static/site.webmanifest" />
-                <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#6c63ff" />
+                <link rel="apple-touch-icon" sizes="76x76" href={appletouchicon} />
+                <link rel="icon" type="image/png" sizes="32x32" href={favIconLarge} />
+                <link rel="icon" type="image/png" sizes="16x16" href={favIconSmall} />
+                <link rel="mask-icon" href={maskIcon} color="#6c63ff" />
                 <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta name="theme-color" content="#6c63ff" />
             </Helmet>
